b2704c872bb28de4cb2a9da4851f3b57a28b986d
isalim079 <salimreza1025@gmail.com>
2024-09-23 11:14:40 +0600

    first commit

---
+++ L2B3-nextjs-authentication-starter-pack/src/app/(withDashboard)/dashboard/page.tsx
@@ -0,0 +1,33 @@
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+
+  console.log(session);
+
+  return (
+    <div>
+      {session?.user && (
+        <>
+          <h1 className="text-4xl text-center mt-10">
+            Welcome to {session?.user?.name}
+          </h1>
+          <h1 className="text-4xl text-center mt-10">
+            Email: {session?.user?.email}
+          </h1>
+          <Image
+            src={session?.user?.image}
+            alt="user image"
+            height={100}
+            width={100}
+            className="mx-auto rounded-full mt-5"
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default DashboardPage;
